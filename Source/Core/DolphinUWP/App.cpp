256f4b4c2459b5683afe3b9f79453f20b8cfb07f
tunip3 <26260613+tunip3@users.noreply.github.com>
2022-12-05 21:41:30 +0000

    fix spacing

--- Source/Core/DolphinUWP/App.cpp
+++ Source/Core/DolphinUWP/App.cpp
@@ -8,7 +8,7 @@
 #include <winrt/Windows.UI.Composition.h>
 #include <winrt/Windows.UI.Core.h>
 #include <winrt/Windows.UI.Input.h>
-#include<winrt/windows.graphics.display.core.h>
+#include <winrt/windows.graphics.display.core.h>
 
 #include <Gamingdeviceinformation.h>
 
